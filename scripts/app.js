a0969688f093ff1d63c4011dd5cc477ccc8815d2
Shawn McKee <shawnroosemckee@gmail.com>
2023-10-13 18:25:43 -0400

    finished adding the icons and if else statements

--- scripts/app.js
+++ scripts/app.js
@@ -14,9 +14,49 @@ const updateUI = (data) => {
     if(cityWeather.WeatherIcon ===1) {
         icon = './images/01-s.png';
     } 
-    else if(cityWeather.WeatherIcon <= 2 && cityWeather.WeatherIcon <= 5 ) {
+    else if(cityWeather.WeatherIcon >= 2 && cityWeather.WeatherIcon <= 5 ) {
         icon = './images/02-s.png';
     }
+    else if(cityWeather.WeatherIcon === 6) {
+        icon = './images/06-s.png';
+    }
+    else if(cityWeather.WeatherIcon === 7 || cityWeather.WeatherIcon === 8 ) {
+        icon = './images/07-s.png';
+    }
+    else if(cityWeather.WeatherIcon === 11) {
+        icon = './images/011-s.png';
+    }
+    else if(cityWeather.WeatherIcon >= 12  && cityWeather.WeatherIcon <= 18 ) {
+        icon = './images/12-s.png';
+    }
+    else if(cityWeather.WeatherIcon >= 19 && cityWeather.WeatherIcon <= 29 ) {
+        icon = './images/22-s.png';
+    }
+    else if(cityWeather.WeatherIcon === 30) {
+        icon = './images/30-s.png';
+    }
+    else if(cityWeather.WeatherIcon === 31) {
+        icon = './images/31-s.png';
+    }
+    else if(cityWeather.WeatherIcon === 32 ) {
+        icon = './images/32-s.png';
+    }
+    else if(cityWeather.WeatherIcon >= 33 && cityWeather.WeatherIcon <= 36 ) {
+        icon = './images/34-s.png';
+    }
+    else if(cityWeather.WeatherIcon === 37 || cityWeather.WeatherIcon === 38 ) {
+        icon = './images/38-s.png';
+    }
+    else if(cityWeather.WeatherIcon >= 39 && cityWeather.WeatherIcon <= 42 ) {
+        icon = './images/39-s.png';
+    } 
+    else if(cityWeather.WeatherIcon === 43 || cityWeather.WeatherIcon === 44 ) {
+        icon = './images/43-s.png';
+    } else {
+        icon = ''
+    }
+
+
 
     details.innerHTML = `
 
